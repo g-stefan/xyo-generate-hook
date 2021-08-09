f284ac4108b1302d2bc14e748cfa2dd392a121cc
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 20:27:16 +0300

    Update

--- source/xyo-generate-hook-version.cpp
+++ source/xyo-generate-hook-version.cpp
@@ -12,10 +12,10 @@
 namespace XYOGenerateHook {
 	namespace Version {
 
-		static const char *version_ = "1.6.0";
-		static const char *build_ = "9";
-		static const char *versionWithBuild_ = "1.6.0.9";
-		static const char *datetime_ = "2021-08-05 20:38:19";
+		static const char *version_ = "1.7.0";
+		static const char *build_ = "18";
+		static const char *versionWithBuild_ = "1.7.0.18";
+		static const char *datetime_ = "2021-08-09 17:38:07";
 
 		const char *version() {
 			return version_;
