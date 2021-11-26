4bb8e2ea34f4a50c5f1bb37a64694684c0b4bdd1
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:25:18 +0200

    Update

--- build/msvc.config.cmd
+++ build/msvc.config.cmd
@@ -3,6 +3,4 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> version-dependency-update xyo-generate-hook
-
-xyo-cc --update-version-dependency xyo-generate-hook
+set BUILD_PROJECT=xyo-generate-hook
--- build/platform/msvc.vendor.cmd
+++ build/platform/msvc.vendor.cmd
@@ -3,8 +3,4 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> clean xyo-generate-hook
-
-if exist output\ rmdir /Q /S output
-if exist temp\ rmdir /Q /S temp
-
+echo - %BUILD_PROJECT% ^> vendor
